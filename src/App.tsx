fbf7569b1e4a52089335f7ecac05360959725607
anhltsefpt <anhltse.fpt@gmail.com>
2026-03-24 09:46:50 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ const App = () => (
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/handbook/:state" element={<Handbook />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
